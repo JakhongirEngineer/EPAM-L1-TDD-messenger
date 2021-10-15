8f87c3c9eb1b89e65f442d6745244c83a6e56d0d
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-15 23:54:46 +0500

    creates ConsoleDriver and Main entrypoint classes

--- src/main/java/com/epam/ld/module2/testing/drivers/ConsoleDriver.java
+++ src/main/java/com/epam/ld/module2/testing/drivers/ConsoleDriver.java
@@ -1,9 +1,25 @@
 package com.epam.ld.module2.testing.drivers;
 
+import com.epam.ld.module2.testing.Client;
+import com.epam.ld.module2.testing.MailServer;
+import com.epam.ld.module2.testing.Messenger;
+import com.epam.ld.module2.testing.template.Template;
+import com.epam.ld.module2.testing.template.TemplateEngine;
+
 public class ConsoleDriver implements Driver{
 
     @Override
     public void run() {
         System.out.println("Console driver is running.");
+
+        MailServer mailServer = new MailServer();
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        Messenger messenger = new Messenger(mailServer, templateEngine);
+
+        Client client = new Client();
+        Template template = new Template();
+
+        messenger.sendMessage(client, template);
     }
 }
