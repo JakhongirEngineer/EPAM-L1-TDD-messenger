10d7d4de63c96db789550eb82e3c350a33a6c127
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-15 23:45:29 +0500

    creates Main entrypoint class

---
+++ src/main/java/com/epam/ld/module2/testing/Main.java
@@ -0,0 +1,33 @@
+package com.epam.ld.module2.testing;
+
+import com.epam.ld.module2.testing.drivers.Driver;
+import com.epam.ld.module2.testing.drivers.DriverFactory;
+import com.epam.ld.module2.testing.drivers.enums.DriverType;
+
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println("You need to choose application mode.");
+        System.out.println("Enter 1 for CONSOLE mode");
+        System.out.println("Enter 2 for FILE mode");
+
+        DriverFactory driverFactory = new DriverFactory();
+        Driver driver;
+        while (true){
+            int modeOption = scanner.nextInt();
+            if (modeOption == 1){
+                driver = driverFactory.createDriver(DriverType.CONSOLE);
+                break;
+            } else if (modeOption == 2){
+                driver = driverFactory.createDriver(DriverType.FILE);
+                break;
+            } else {
+                System.out.println("You need to choose either CONSOLE (1) or FILE (2) option");
+            }
+        }
+        driver.run();
+    }
+}
--- src/main/java/com/epam/ld/module2/testing/drivers/ConsoleDriver.java
+++ src/main/java/com/epam/ld/module2/testing/drivers/ConsoleDriver.java
@@ -1,6 +1,7 @@
 package com.epam.ld.module2.testing.drivers;
 
 public class ConsoleDriver implements Driver{
+
     @Override
     public void run() {
         System.out.println("Console driver is running.");
