ff6f55af35a3465782b4699934f51c5956fb218c
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-16 00:17:10 +0500

    implements TemplateEngine.generateMessage

--- src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
+++ src/main/java/com/epam/ld/module2/testing/template/TemplateEngine.java
@@ -2,6 +2,8 @@ package com.epam.ld.module2.testing.template;
 
 import com.epam.ld.module2.testing.Client;
 
+import java.util.Map;
+
 /**
  * The type Template engine.
  */
@@ -14,6 +16,20 @@ public class TemplateEngine {
      * @return the string
      */
     public String generateMessage(Template template, Client client) {
-        return null;
+        Map<String, String> providedKeyValues = client.getProvidedKeyValues(); // example: name => Alica, job => Software Engineer
+        Map<String, String> keyValuesInTemplate = template.getKeyValues(); // example: name => null, job => null
+        String generatedMessage = template.getBody(); // initial template body
+        int providedKeyValueCounter = 0; // all placeholders must be provided, so counter is used to keep track of them
+        for (String providedKey : providedKeyValues.keySet()){
+            if (keyValuesInTemplate.containsKey(providedKey)) {
+                generatedMessage = generatedMessage.replace("#{" +providedKey + "}", providedKeyValues.get(providedKey));
+                providedKeyValueCounter++;
+            }
+        }
+        if (keyValuesInTemplate.size() == providedKeyValueCounter){
+            return generatedMessage;
+        } else {
+            throw new RuntimeException("not each key is given");
+        }
     }
 }
