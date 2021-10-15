d2243254de417e688207fe74548e6b76083b3f66
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-16 00:15:11 +0500

    implements Template.java class

--- src/main/java/com/epam/ld/module2/testing/Main.java
+++ src/main/java/com/epam/ld/module2/testing/Main.java
@@ -8,24 +8,21 @@ import java.util.Scanner;
 
 public class Main {
     private final static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         System.out.println("You need to choose application mode.");
         System.out.println("Enter 1 for CONSOLE mode");
         System.out.println("Enter 2 for FILE mode");
 
         DriverFactory driverFactory = new DriverFactory();
         Driver driver;
-       // ModeSingleton modeSingleton = ModeSingleton.getModeSingleton();
         while (true){
             int modeOption = scanner.nextInt();
             if (modeOption == 1){
                 driver = driverFactory.createDriver(DriverType.CONSOLE);
-              //  modeSingleton.setDriverType(DriverType.CONSOLE);
                 break;
             } else if (modeOption == 2){
                 driver = driverFactory.createDriver(DriverType.FILE);
-              //  modeSingleton.setDriverType(DriverType.FILE);
                 break;
             } else {
                 System.out.println("You need to choose either CONSOLE (1) or FILE (2) option");
--- src/main/java/com/epam/ld/module2/testing/template/Template.java
+++ src/main/java/com/epam/ld/module2/testing/template/Template.java
@@ -1,7 +1,77 @@
 package com.epam.ld.module2.testing.template;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * The type Template.
  */
 public class Template {
+
+    private String body;
+    private Map<String, String> keyValues;
+
+    public Template() {
+        this.body = "";
+        this.keyValues = new HashMap<>();
+    }
+
+    public Template(String body) {
+        this.body = body;
+        this.keyValues = new HashMap<>();
+    }
+
+    public Template(String body, Map<String, String> keyValues) {
+        this.body = body;
+        this.keyValues = keyValues;
+    }
+
+    private void initializeKeysInKeyValues(){
+        // example: "Thank you Mr/Ms #{name}. You will be working as a #{occupation}"
+        // result in keyValues: name => null, occupation => null; because they are not provided yet.
+        List<String> keys = extractKeysFromContext();
+        keys.forEach(k -> keyValues.put(k, null));
+    }
+
+    private List<String> extractKeysFromContext(){
+        List<String> keys = new ArrayList<>();
+        StringBuilder currentKey = new StringBuilder();
+        int i = 0;
+        while ( i < body.length() ) {
+            if (i < body.length() - 3 && body.charAt(i) == '#' && body.charAt(i+1) == '{') {
+                i += 2; // skip
+                while ( i < body.length() && body.charAt(i) != '}') {
+                    currentKey.append(body.charAt(i++));
+                }
+                if (i < body.length() && body.charAt(i) == '}'){
+                    keys.add(currentKey.toString());
+                    currentKey = new StringBuilder(); // reinitialize for a new key
+                }
+            } else {
+                i++;
+            }
+        }
+        return keys;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+        this.keyValues = new HashMap<>();
+        initializeKeysInKeyValues();
+    }
+
+    public Map<String, String> getKeyValues() {
+        return keyValues;
+    }
+
+    public void setKeyValues(Map<String, String> keyValues) {
+        this.keyValues = keyValues;
+    }
+
 }
