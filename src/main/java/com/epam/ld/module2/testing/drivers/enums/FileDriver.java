06f56e56e7f7677061ab44686dbf4d984f907472
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-15 23:37:13 +0500

    creates FileDriver.java that implements Driver interface

---
+++ src/main/java/com/epam/ld/module2/testing/drivers/enums/FileDriver.java
@@ -0,0 +1,8 @@
+package com.epam.ld.module2.testing.drivers.enums;
+
+public class FileDriver implements Driver{
+    @Override
+    public void run() {
+        System.out.println("File driver is running");
+    }
+}
