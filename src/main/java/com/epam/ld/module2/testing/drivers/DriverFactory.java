e88f5c172f95c69b3b5f5180add07b3319a5b81a
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-15 23:42:28 +0500

    creates DriverFactory.java class

--- src/main/java/com/epam/ld/module2/testing/drivers/DriverFactory.java
+++ src/main/java/com/epam/ld/module2/testing/drivers/DriverFactory.java
@@ -1,4 +1,16 @@
 package com.epam.ld.module2.testing.drivers;
 
+import com.epam.ld.module2.testing.drivers.enums.DriverType;
+
 public class DriverFactory {
+
+    public Driver createDriver(DriverType type){
+        switch (type){
+            case CONSOLE:
+                return new ConsoleDriver();
+            case FILE:
+                return new FileDriver();
+        }
+        return null;
+    }
 }
