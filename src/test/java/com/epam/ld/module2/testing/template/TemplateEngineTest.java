8ad5f11dfad235e5833d8c8249713ca0355ef427
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-16 16:56:29 +0500

    tests when Template and Client placeholders have one to one correspondence, generatedMessage is correct.

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,13 +1,26 @@
 package com.epam.ld.module2.testing.template;
 
+import com.epam.ld.module2.testing.Client;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class TemplateEngineTest {
 
+    @Mock
+    Template template;
+    @Mock
+    Client client;
 
     @BeforeEach
     void setUp() {
@@ -20,6 +33,21 @@ class TemplateEngineTest {
     }
 
     @Test
-    void generateMessage() {
+    void whenClientProvidesOnlyAllOfThePlaceholdersGenerateMessageCorrectly() {
+        Map<String,String> clientProvidedKeyValues = new HashMap<>();
+        clientProvidedKeyValues.put("hobby", "reading");
+        clientProvidedKeyValues.put("name", "John");
+        when(client.getProvidedKeyValues()).thenReturn(clientProvidedKeyValues);
+
+        Map<String,String> templateKeyValues = new HashMap<>();
+        templateKeyValues.put("hobby",null);
+        templateKeyValues.put("name",null);
+        when(template.getKeyValues()).thenReturn(templateKeyValues);
+        String templateBody = "Hello, my name is #{name}. I love #{hobby}.";
+        when(template.getBody()).thenReturn(templateBody);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        String generatedMessage = templateEngine.generateMessage(template, client);
+        assertEquals("Hello, my name is John. I love reading.", generatedMessage);
     }
 }
