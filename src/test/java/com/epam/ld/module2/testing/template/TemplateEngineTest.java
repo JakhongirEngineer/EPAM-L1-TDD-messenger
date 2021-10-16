9604c4c0be51cf9ef5ba69322c2294dde5660fb6
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-16 16:38:53 +0500

    adds junit-jupiter-param dependency to achive parametrized testing

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,25 @@
 package com.epam.ld.module2.testing.template;
 
-public class TemplateEngineTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemplateEngineTest {
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void generateMessage() {
+    }
 }
