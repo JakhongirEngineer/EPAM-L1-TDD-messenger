40f61e20a838308e8598593c440731aeac6b3f6b
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-16 16:16:19 +0500

    cleans up TemplateTest.java class

--- src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateTest.java
@@ -1,7 +1,5 @@
 package com.epam.ld.module2.testing.template;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
