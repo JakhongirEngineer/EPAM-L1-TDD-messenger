f2e32f8be0c71cd068eebe56063174d3405ca4d9
Jakhongir Rasulov <jakhongirrasulov007@gmail.com>
2021-10-17 00:27:25 +0500

    modifies FileDriverTest

--- src/test/java/com/epam/ld/module2/testing/drivers/FileDriverTest.java
+++ src/test/java/com/epam/ld/module2/testing/drivers/FileDriverTest.java
@@ -3,9 +3,16 @@ package com.epam.ld.module2.testing.drivers;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,12 +21,11 @@ import static org.mockito.Mockito.when;
 
 class FileDriverTest {
 
-
     @Test
     void whenOnFileModeInvalidFilenamesAreProvidedThrowsAnException()  {
         Scanner scanner = mock(Scanner.class);
         when(scanner.nextLine()).thenReturn("ThereIsNotSuchFileNameSoItShouldThrowAnException").thenReturn("noinputfile");
         FileDriver fileDriver = new FileDriver(scanner);
-        assertThrows(RuntimeException.class, () -> fileDriver.run());
+        assertThrows(RuntimeException.class, fileDriver::run);
     }
 }
